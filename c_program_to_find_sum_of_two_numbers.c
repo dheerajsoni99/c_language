b112cded5fa21f2d89c081602e9fc51dc888d7d7
dheerajsoni99 <122505217+dheerajsoni99@users.noreply.github.com>
2023-10-28 19:29:36 +0530

    Create c_program_to_find_sum_of_two_numbers.c
    In this program we are using two printf and scanf functions for taking two inputs from use \n is for next line of statement.
    In place of using two seperate we can use one function icluding both inputs this is equavalent to the same and works same as written. 
    when using one we put %d%d for taking two inputs and scaning both in one line. for this we will discuss in future programs in same repositery.

---
+++ c_program_to_find_sum_of_two_numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+int a,b,sum;
+printf("enter your first number : \n");
+scanf("%d",&a);
+printf("enter your second number : \n");
+scanf("%d",&b);
+sum = a+b;
+printf("sum of these numbers is  : %d",sum);
+return 0;
+}
