bd5b81748af8d2b7f259f4295570d62a4a7c13d6
dheerajsoni99 <122505217+dheerajsoni99@users.noreply.github.com>
2023-10-31 19:16:54 +0530

    Create c_program_to_check_even_or_odd.c
    % is called the modula functin is normally used to check the divisibility. Here we check divisibility with respect to 2. If a number is divisible by 2 then it is even number and called multiple of two.
    the modula function give the output true for eligible divisible numbers so the code inside of the if will be executed.

---
+++ c_program_to_check_even_or_odd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+  int a;
+printf("Enter your number : \n");
+scanf("%d",&a);
+if(a%2==0){
+printf("the number is even \n");
+}
+else{
+printf("the number is odd \n");
+}
+return 0;
+}
