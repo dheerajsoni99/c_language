669beb41514a9cbf87b1b18c49160022d4b94c46
dheerajsoni99 <122505217+dheerajsoni99@users.noreply.github.com>
2023-10-31 19:45:39 +0530

    Create comparision.c
    the comparision is basic mathematics operation that is used in c language if the comparision is correct then it will output one and if not correct it will output 0 as true and false are not present in c language

---
+++ comparision.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+  int a=10,b=15,c=20;
+printf("%d",a>b);//if the comparision is false then it will give value of 0 if the comparision is true then it will output 1 hence printf will execute only one or zero(for false case).
+return O;
+}
