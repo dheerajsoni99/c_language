53a895e4f566a76f1cd9f3dc8894b2bf23346651
dheerajsoni99 <122505217+dheerajsoni99@users.noreply.github.com>
2023-10-31 20:02:50 +0530

    Create c_program_to_find_average_of_three.c

---
+++ c_program_to_find_average_of_three.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+  int a,b,c,average;
+printf("enter first number : \n);
+  scanf("%d",&a);
+printf("enter second number  : \n);
+  scanf("%d",&b);
+printf("enter third number  : \n);
+  scanf("%d",&c);
+average=(a+b+c)/3;
+printf("the average is %d ",average);
+return 0;
+}
